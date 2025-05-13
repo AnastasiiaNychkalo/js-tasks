b5cc3c2323490fd7cbf553b63b471168e51b6943
AnastasiiaNychkalo <nastyanuc@gmail.com>
2025-05-13 09:35:35 +0300

    feat(codeWarsDaili/12_05) solve tasks

---
+++ codeWarsDaily/12_05.js
@@ -0,0 +1,139 @@
+"use strict";
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this seems too simple for you try the next level
+
+// Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays... 😕
+
+function printArray(array) {
+  return array.join(",");
+}
+
+// Once upon a time, on a way through the old wild mountainous west,…
+// … a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+
+// Going to one direction and coming back the opposite direction right away is a needless effort. Since this is the wild west, with dreadful weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+
+// How I crossed a mountainous desert the smart way.
+// The directions given to the man are, for example, the following (depending on the language):
+
+// ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+// or
+// { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+// or
+// [North, South, South, East, West, North, West]
+// You can immediately see that going "NORTH" and immediately "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply:
+
+// ["WEST"]
+// or
+// { "WEST" }
+// or
+// [West]
+
+function dirReduc(arr) {
+  const opposites = {
+    NORTH: "SOUTH",
+    SOUTH: "NORTH",
+    EAST: "WEST",
+    WEST: "EAST",
+  };
+
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const last = result[result.length - 1];
+    if (opposites[arr[i]] === last) {
+      result.pop();
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {
+  numbers.sort((a, b) => a - b);
+  return numbers[0] + numbers[1];
+}
+
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+String.prototype.isUpperCase = function () {
+  return this.toString() === this.toUpperCase();
+};
+
+// Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+// The Task
+// Think of a way to store the languages as a database. The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+// The Database
+// Please modify this as appropriate for your language.
+
+// [ ("english", "Welcome")
+// , ("czech", "Vitejte")
+// , ("danish", "Velkomst")
+// , ("dutch", "Welkom")
+// , ("estonian", "Tere tulemast")
+// , ("finnish", "Tervetuloa")
+// , ("flemish", "Welgekomen")
+// , ("french", "Bienvenue")
+// , ("german", "Willkommen")
+// , ("irish", "Failte")
+// , ("italian", "Benvenuto")
+// , ("latvian", "Gaidits")
+// , ("lithuanian", "Laukiamas")
+// , ("polish", "Witamy")
+// , ("spanish", "Bienvenido")
+// , ("swedish", "Valkommen")
+// , ("welsh", "Croeso")
+// ]
+// Possible invalid inputs include:
+
+// IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+// IP_ADDRESS_NOT_FOUND - ip address not in the database
+// IP_ADDRESS_REQUIRED - no ip address was supplied
+
+function greet(language) {
+  const BD = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  return language in BD ? BD[language] : BD["english"];
+}
